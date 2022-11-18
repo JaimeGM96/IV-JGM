b7ed460321a8d3d6e3c79b4e64447a3c51edc620
JaimeGM96 <jaimegallardo@correo.ugr.es>
2022-11-18 13:10:36 +0100

    build(grunt): Añadida la tarea install (#20)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,8 @@ module.exports = function(grunt) {
     grunt.registerTask("check", function () {
         "tsc --noEmit src/*.ts";
     });
+
+    grunt.registerTask("install", function () {
+        "yarn install";
+    });
 }
